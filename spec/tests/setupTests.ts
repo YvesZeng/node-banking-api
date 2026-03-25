e797b663d8904e119ee999f2373488a3e54823a1
kyle <apple@appledeMacBook-Pro.local>
2026-03-25 23:38:13 +0800

    fix: seed customers in beforeAll instead of afterEach

--- spec/tests/setupTests.ts
+++ spec/tests/setupTests.ts
@@ -9,18 +9,18 @@ module.exports = {
         beforeAll(() => {
             process.env.SQLITE_DB_PATH = ':memory:';
             db = getDatabase();
-        });
-
-        afterEach(() => {
-            db.exec('DELETE FROM transfers');
-            db.exec('DELETE FROM bank_accounts');
-            db.exec('DELETE FROM customers');
             
+            // Seed initial customers
             const insert = db.prepare('INSERT INTO customers (name) VALUES (?)');
             const names = ['Arisha Barron', 'Branden Gibson', 'Rhonda Church', 'Georgina Hazel'];
             for (const name of names) {
                 insert.run(name);
             }
+        });
+
+        afterEach(() => {
+            db.exec('DELETE FROM transfers');
+            db.exec('DELETE FROM bank_accounts');
         });
 
         afterAll(() => {
